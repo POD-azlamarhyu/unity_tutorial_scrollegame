c444c2584aa9933cbfe4996f9b7ea4dc427ce2e4
Jiro <adrague.ggoch@gmail.com>
2022-04-01 10:30:18 +0900

    first commit

---
+++ UnityChanBallRoll-master/Assets/PlayerController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rb;
+    public float speed;
+    int count;
+    public Text cnttex;
+    AudioSource se;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        setCount();
+        se = GetComponent<AudioSource>();
+        // Debug.Log("Hellor world");
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float moveH = Input.GetAxis("Horizontal");
+        float moveV = Input.GetAxis("Vertical");
+
+        Vector3 move = new Vector3(moveH,0,moveV);
+        rb.AddForce(move * speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("item"))
+        {
+            other.gameObject.SetActive(false);
+            count += 1;
+            setCount();
+            se.Play();
+        }else if(other.gameObject.CompareTag("buttom"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+
+        }
+        
+    }
+
+    private void setCount()
+    {
+        cnttex.text = count.ToString() + " Get!!";
+    }
+}
---
+++ UnityChanBallRoll-master/Assets/goalChecker.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class goalChecker : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject unitychan;
+    public AudioSource goalBGM;
+    public AudioSource gameBGM;
+    public GameObject retryButton;
+    void Start()
+    {
+        retryButton.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        other.gameObject.GetComponent<Rigidbody>().isKinematic = true;
+        unitychan.transform.LookAt(Camera.main.transform);
+        unitychan.GetComponent<Animator>().SetTrigger("goal");
+        gameBGM.Stop();
+        goalBGM.Play();
+        retryButton.SetActive(true);
+
+    }
+
+    public void Retry()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
